ab66f0c19443d39c1043c307c89fa6bb3c560139
Lucas Eduardo <lukaszonecell@gmail.com>
2023-07-24 16:31:56 -0300

    First client method

--- lib/whats_app/client.rb
+++ lib/whats_app/client.rb
@@ -6,5 +6,27 @@ module WhatsApp
       @number_identification = number_identification
       @token = token
     end
+
+    def connection
+      @connection ||= Faraday.new(url: "https://graph.facebook.com/v17.0/#{number_identification}") do |faraday|
+        faraday.request :json
+        faraday.response :json
+        faraday.request :authorization, 'Bearer', token
+      end
+    end
+
+    def send_message(phone_number:, message:)
+      body = {
+        "messaging_product": "whatsapp",
+        "recipient_type": "individual",
+        "to": phone_number,
+        "type": "text",
+        "text": {
+          "preview_url": false,
+          "body": message
+        }
+      }
+      connection.post("messages", body.to_json)
+    end
   end
 end
